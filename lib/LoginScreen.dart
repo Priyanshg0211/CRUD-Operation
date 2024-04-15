368b1de42b7d3d5f46883d71bbd4c96fe2711579
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-04-16 01:04:00 +0530

    first commit

---
+++ lib/LoginScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Loginpage extends StatefulWidget {
+  const Loginpage({super.key});
+
+  @override
+  State<Loginpage> createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login',style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
